20a659685323c3f17771210baac0284d946fb848
Sebastian Beschke <290059+sbeschke@users.noreply.github.com>
2026-06-21 16:55:59 +0200

    Fix tests + run tests before committing

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("mops.urls", namespace="mops")),
+    path("mops/", include("mops.urls", namespace="mops")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
